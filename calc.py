069114a90b2276e41b97ab57a045695217f66ed2
job <akungajob47@gmail.com>
2023-02-15 10:46:06 +0300

    calc.py

---
+++ calc.py
@@ -0,0 +1,21 @@
+#calculator project
+num1=eval(input("enter first number:"))
+num2=eval(input("enter second number:"))
+
+operator=input("enter operator:")
+def  add(num1,num2):
+    result=num1+num2
+    return result
+
+
+def subtract(num1,num2):
+    result=num1-num2
+    return result
+
+def divide(num1,num2):
+  result=num1/num2
+  return result
+
+def multiply(num1,num2):
+   result=num1*num2
+   return result
